0730540c7510bfd21287fd2d50d7dbf9ef9613f1
Kian Ahmadi <otyyjhct@gmail.com>
2025-12-09 22:07:02 +0330

    Reading from the TcpStream and printing the data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // ToDo: try a couple more times to connect and don't panic for a few times.
+    let listener = TcpListener::bind("127.0.0.1:7878")
+        .expect("failed to bind to the local address. the port is probably busy");
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = stream.expect("failed to read the stream");
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.expect("Failed to read http request lines"))
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("Request: {:#?}", http_request);
 }
