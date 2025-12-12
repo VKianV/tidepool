bb1f781dd7199b05645fc33b8fa8d98101e1b6fd
Kian Ahmadi <otyyjhct@gmail.com>
2025-12-12 19:20:22 +0330

    Spawning a new thread for each stream

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,16 @@ use std::{
 };
 
 fn main() {
+    println!("tidepool has started listening for connections on 127.0.0.1:7878 address");
     // ToDo: try a couple more times to connect and don't panic for a few times.
     let listener = TcpListener::bind("127.0.0.1:7878")
         .expect("failed to bind to the local address. the port is probably busy");
 
     for stream in listener.incoming() {
         let stream = stream.expect("failed to read the stream");
+        println!("incoming request");
 
-        handle_connection(stream);
+        thread::spawn(|| handle_connection(stream));
     }
 }
 
