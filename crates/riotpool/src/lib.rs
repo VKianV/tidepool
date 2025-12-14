7c16fcb1ce72039a75d6b0e04a53bfe2f2dddd1d
Kian Ahmadi <otyyjhct@gmail.com>
2025-12-14 23:53:53 +0330

    Sharing the receiver among the Worker instances using Arc and Mutex

--- crates/riotpool/src/lib.rs
+++ crates/riotpool/src/lib.rs
@@ -17,13 +17,15 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-        
-        let mut workers = Vec::with_capacity(size);
 
         let (sender, receiver) = mpsc::channel();
 
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
         for id in 1..=size {
-            workers.push(Worker::new(id, receiver));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         Self { workers, sender }
@@ -42,7 +44,7 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: mpsc::Receiver<Job> ) -> Self {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>> ) -> Self {
         let thread = thread::spawn(|| {
             receiver;
         });
