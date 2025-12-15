c0aef3c58c1ed6d5852f1533e5fde980a004d334
Kian Ahmadi <otyyjhct@gmail.com>
2025-12-15 15:19:42 +0330

    implementing the mpmc version of the same code base though it is important to know that this featrue is in rust nightly

--- crates/riotpool/src/lib.rs
+++ crates/riotpool/src/lib.rs
@@ -1,10 +1,11 @@
-use std::{thread, sync::{mpsc, Arc, Mutex}};
+#![feature(mpmc_channel)]
+use std::{thread, sync::{mpmc, Arc, Mutex}};
 
 struct Job;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpmc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -17,15 +18,14 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
-
-        let (sender, receiver) = mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
+        
         let mut workers = Vec::with_capacity(size);
 
+        let (sender, receiver) = mpmc::channel();
+
+
         for id in 1..=size {
-            workers.push(Worker::new(id, Arc::clone(&receiver)));
+            workers.push(Worker::new(id, receiver.clone()));
         }
 
         Self { workers, sender }
@@ -44,7 +44,7 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>> ) -> Self {
+    fn new(id: usize, receiver: mpmc::Receiver<Job> ) -> Self {
         let thread = thread::spawn(|| {
             receiver;
         });
